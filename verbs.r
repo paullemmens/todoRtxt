0157af38b2cab936d09da69999d0854ec508207b
Paul Lemmens <paul.lemmens@philips.com>
2018-09-10 13:22:16 +0200

    Adds file with future verbs for the client.

---
+++ verbs.r
@@ -0,0 +1,24 @@
+#    add|a "THING I NEED TO DO +project @context"
+#    addm "THINGS I NEED TO DO
+#          MORE THINGS I NEED TO DO"
+#    addto DEST "TEXT TO ADD"
+#    append|app ITEM# "TEXT TO APPEND"
+#    archive
+#    command [ACTIONS]
+#    deduplicate
+#    del|rm ITEM# [TERM]
+#    depri|dp ITEM#[, ITEM#, ITEM#, ...]
+#    do ITEM#[, ITEM#, ITEM#, ...]
+#    help [ACTION...]
+#    list|ls [TERM...]
+#    listall|lsa [TERM...]
+#    listaddons
+#    listcon|lsc [TERM...]
+#    listfile|lf [SRC [TERM...]]
+#    listpri|lsp [PRIORITIES] [TERM...]
+#    listproj|lsprj [TERM...]
+#    move|mv ITEM# DEST [SRC]
+#    prepend|prep ITEM# "TEXT TO PREPEND"
+#    pri|p ITEM# PRIORITY
+#    replace ITEM# "UPDATED TODO"
+#    report
