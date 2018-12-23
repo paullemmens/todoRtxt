09d3d9a7becf48fbd5807ef01131366c3a02ec02
Paul <paul.lemmens@gmail.com>
2018-12-23 18:38:16 +0100

    Add unit tests for projects parser

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -21,4 +21,28 @@ test_that("contexts are parsed correctly", {
   expect_identical(parse_tags(todo2, '@'), res2)
   expect_identical(parse_tags(todo3, '@'), res3)
 })
+
+
+context("Test projects parser")
+
+test_that('projects are parsed correctly', {
+  todo1 <- '+proj xxx'
+  res1 <- tibble::tibble(context = character(), start = integer(), end = integer())
+
+  todo2 <- ' +proj xxx'
+  res2 <- tibble::tribble(
+    ~context,    ~start, ~end,
+    '+proj',  1L,     6L
+  )
+
+  todo3 <- 'xxx +proj2 yyy +proj3 zzz'
+  res3 <- tibble::tribble(
+    ~context,    ~start, ~end,
+    '+proj2', 4L,     10L,
+    '+proj3', 15L,    21L
+  )
+
+  expect_identical(parse_tags(todo1, '+'), res1)
+  expect_identical(parse_tags(todo2, '+'), res2)
+  expect_identical(parse_tags(todo3, '+'), res3)
 })
