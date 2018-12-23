34ba64963a89ced5af1d601ba7ffcd8e3a600e38
Paul <paul.lemmens@gmail.com>
2018-12-23 18:37:27 +0100

    Refactor context parser to generic tags parser
    There is only one character difference in how contexts (@) and
    projects (+) are defined so parser can be reused.

--- R/parser.R
+++ R/parser.R
@@ -1,11 +1,14 @@
-#' @title Parse Contexts In A Todo
+#' @title Parse Tags From a Todo
 #'
 #' @description
-#' Contexts are tags prefixed with an `@`. One todo can contain multiple
-#' contexts that should all be preserved. The regular expression is taken
-#' from the source code of Simpletask by Mark Janssen.
+#' Contexts are tags prefixed with an `@`. Projects are tags prefixed with
+#' a `+` (plus). One todo can contain multiple
+#' contexts and/or multiple projects that should all be preserved.
+#' The regular expressions are taken from the source code of Simpletask by
+#' Mark Janssen.
 #'
 #' @param todo A single todo (string).
+#' @param tag A plus `+` or `@` sign (string)
 #'
 #' @return A (mini) data frame with a row for each context found and in that
 #'     row a variable for the context and its start and end position in the
@@ -14,8 +17,8 @@
 #' @importFrom dplyr "%>%"
 #'
 #' @export
-parse_contexts <- function(todo) {
-  contexts_pattern <- '( \\@\\w+)'
+parse_tags <- function(todo, tag) {
+  contexts_pattern <- paste0('( \\', tag, '\\w+)')
 
   contexts <- stringr::str_extract_all(todo, pattern = contexts_pattern)[[1]] %>%
     stringr::str_trim()
@@ -28,3 +31,4 @@ parse_contexts <- function(todo) {
 
   return(contexts)
 }
+
