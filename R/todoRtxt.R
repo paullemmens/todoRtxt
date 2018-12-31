3ad02d06cbffb4cc296632f3597a2f7dd6541ef2
Paul Lemmens <paul.lemmens@gmail.com>
2018-12-31 14:52:23 +0100

    Add package description

---
+++ R/todoRtxt.R
@@ -0,0 +1,27 @@
+#' @title todoRtxt: An R Parser And Interaction Toolkit For todo.txt Files
+#'
+#' @author Paul Lemmens (paul.lemmens@gmail.com)
+#'
+#' @description
+#' `todo.txt` files are flat (ascii) text files containing a series of to dos
+#' complying to a format specified by Gina Trappani (see [todotxt.org]) that
+#' enables decorating a to with priority, due date, and tags for sorting and
+#' filtering.
+#'
+#' The `todoRtxt` R package provides a parser for such files as
+#' well as functions to add, update, delete, or cross out completed to dos.
+#' A Shiny app is provided that provides a (semi) GUI for these operations as
+#' well as an analytics engine for completed tasks.
+#'
+#' @section Overview
+#' A short section on how the package can be used.
+#'
+#' @section Exported functions
+#' Loading one/more files, editing/modifying a to do.
+#'
+#' @section Shiny app
+#' **FIXME: todo**
+#'
+#' @docType package
+#' @name todoRtxt
+NULL
