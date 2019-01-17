11a0e403db97b981e50a142c36ed34f2a2308159
Paul Lemmens <paul.lemmens@philips.com>
2019-01-17 08:56:42 +0100

    Rewrite modify_task to modify_property
    This mirrors my thinking that for now it is better to keep the two
    functions completely separate despite that they do mirror their
    approach and thus also duplicates code.

--- R/update.R
+++ R/update.R
@@ -1,4 +1,4 @@
-#' @title Modify A Tasks
+#' @title Modify Task Property
 #'
 #' @author Paul Lemmens (paul.lemmens@gmail.com)
 #'
@@ -10,6 +10,7 @@
 #'
 #' The function presumes that all modifications of properties can be based
 #' on strings like "prop:value" (wherein underscores represent a space).
+#' Thus, strictly speaking, tags are not properties of a task.
 #'
 #' To implement a degree of flexibility in how to specify properties and
 #' values, `prop` defaults to NULL so that `value` can (also) be used to
@@ -23,21 +24,32 @@
 #' @param task A string with one (or, as vector, one or more) task(s) that
 #'     follow(s) the todo.txt specification by Gina Trapani. When multiple 
 #'     tasks are provided, *all* tasks are modified in the same way.
-#' @param prop Identifier of the property of the task (a string).
-#' @param value New value for the property.
+#' @param prop Identifier of the property of the task (a string). Can be
+#'     postfixed with a colon (e.g. "due:" instead of "due"). Defaults to
+#'     NULL implying that then `value` needs to be specified as
+#'     `property:value` instead of only `value`.
+#' @param value New value for the property (a string).
 #'
 #' @return The updated task(s)
 #'
-modify_task <- function(task, prop = NULL, value) {
+#' @name modify_property
+modify_property <- function(task, prop = NULL, value) {
 
   if (is.null(prop)) {
     prop <- stringr::str_split(value, ':', simplify = TRUE)[1, 1]
   } else {
-    value <- paste0(prop, ':', value)
+    if (stringr::str_sub(prop, start = -1, end = -1) == ':') {
+      value <- paste0(prop, value)
+    } else {
+      value <- paste0(prop, ':', value)
+    }
   }
 
+  ## TODO: ugly, redoes stringr::str_sub() from above which makes errors likely
+  prop <- dplyr::if_else(stringr::str_sub(prop, -1, -1) == ':', prop, paste0(prop, ':'))
+
   task <- stringr::str_replace(task,
-                               pattern     = paste0(prop, ':[:graph:]+'),
+                               pattern     = paste0(prop, '[:graph:]+'),
                                replacement = value)
 
   return(task)
--- tests/testthat/test-task-updating.R
+++ tests/testthat/test-task-updating.R
@@ -1,15 +1,19 @@
-context("test-task-updating")
+context("Modify task properties")
+
+tasks <- c('2018-12-12 my new task +tag due:2019-01-12 @work',
+           'another new task @work due:2019-01-11',
+           'due:2019-01-14 what +tag i need to do +job')
 
 test_that("property modification works", {
-  tasks <- c('2018-12-12 my new task due:2019-01-12 @work',
-             'another new task @work due:2019-01-11',
-             'due:2019-01-14 what i need to do')
-  new_value <- 'due:2020-12-31'
-  expected <- c('2018-12-12 my new task due:2020-12-31 @work',
-                'another new task @work due:2020-12-31',
-                'due:2020-12-31 what i need to do')
 
-  expect_equal(modify_task(tasks[1], value = new_value), expected[1])
-  expect_equal(modify_task(tasks[1], prop = 'due', value = '2020-12-31'), expected[1])
-  expect_equal(modify_task(tasks, value = new_value), expected)
+  expected <- c('2018-12-12 my new task +tag due:2020-12-31 @work',
+                'another new task @work due:2020-12-31',
+                'due:2020-12-31 what +tag i need to do +job')
+
+  expect_equal(modify_property(tasks[1], value = 'due:2020-12-31'), expected[1])
+  expect_equal(modify_property(tasks[1], prop = 'due:', value = '2020-12-31'), expected[1])
+  expect_equal(modify_property(tasks[1], prop = 'due', value = '2020-12-31'), expected[1])
+  expect_equal(modify_property(tasks, value = 'due:2020-12-31'), expected)
+})
+
 })
