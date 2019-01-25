8051836450b21d9318855d39e15454afbc3ec61b
Paul Lemmens <paul.lemmens@gmail.com>
2019-01-25 22:15:41 +0100

    Implement safely writing tasks to file

--- R/file_operations.R
+++ R/file_operations.R
@@ -44,16 +44,50 @@ load_tasks <- function(f) {
 write_tasks <- function(tasks, todo_file) {
   stopifnot(length(tasks) > 0)
 
-  ## FIXME: expand to use tryCatch()es and a backup of the original file.
-  ## for that use the fs package??
-
   ## 1. Write new file to temp local location
-  ## 2. if successful temp file, then create backup copy of original
-  ## 3. overwrite original with new file from temp locatio
-  ## 4. remove backup of original
+  tempfile <- fs::file_create(fs::file_temp())
+  if (fs::file_exists(tempfile)) {
+    writeLines(tasks, con = tempfile)
+  }
 
+  ## 2. if successful temp file, then create backup copy of original
   ## FIXME: check if it is feasible to (first) parse temporary (new) file to
   ## validate new content against old changed content.
+  bakfile <- fs::file_temp()
+  tryCatch({
+    fs::file_copy(todo_file, bakfile)
+  }, warning = function(warn) {
+      message('Something did not work out making a backup of the original file.')
+      message(warn)
+      return(NULL)
+  }, error = function(err) {
+      message('Something did not work out making a backup of the original file.')
+      message(err)
+      return(NA)
+  })
 
-  writeLines(tasks, con = todo_file)
+  ## 3. overwrite original with new file from temp location
+  ## catching errors to prevent losing info.
+  tryCatch({
+    fs::file_copy(tempfile, todo_file, overwrite = TRUE)
+  },
+  warning = function(war) {
+    message('Warning during writing todo file. Restoring backup.')
+    message(warn)
+    fs::file_copy(bakfile, todo_file, overwrite = TRUE)
+    return(NULL)
+  },
+  error = function(err) {
+    message('Error during writing todo file. Restoring backup.')
+    message(err)
+    fs::file_copy(bakfile, todo_file, overwrite = TRUE)
+    return(NA)
+  })
+
+  ## 4. remove backup of original
+  fs::file_delete(c(tempfile, bakfile))
+
+  return(TRUE)
 }
+
+
