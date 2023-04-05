9880a7beafc8283d93bbf750cb00b903f5f281df
Paul Lemmens <paul.lemmens@gmail.com>
2023-04-05 16:10:09 +0200

    Adjust renaming to output from as_tibble()

--- R/parser.R
+++ R/parser.R
@@ -124,10 +124,10 @@ parse_recurrence <- function(todo) {
 parse_prefixes <- function(todo) {
   prefix_pattern <- '(^x )?(\\([A-Z]\\) )?(\\d{4}-\\d{2}-\\d{2} +)?(\\d{4}-\\d{2}-\\d{2} +)?(\\d{4}-\\d{2}-\\d{2} +)*(.*)' # nolint
 
-  names(prefixes)[-6] <- c('task', 'done', 'priority', 'date_completed',
-                           'date_created', 'task_cleaned')
   prefixes <- tibble::as_tibble(stringr::str_match(todo, pattern = prefix_pattern),
                                 .name_repair = ~ vctrs::vec_as_names(..., repair = "unique", quiet = TRUE)) # nolint
+  names(prefixes) <- c('task', 'done', 'priority', 'date_completed', 'date_created',
+                       'V6', 'task_cleaned')
 
   prefixes <- prefixes %>%
     dplyr::select(-V6) %>%
